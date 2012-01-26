ae150ba31f33e7a1c574f4698724ea1a6fd3e7b0
Joshua Reetz <thetux2@gmail.com>
2012-01-26 16:58:12 -0700

    Updated to 1.1. Improved speed by caching smoke size

--- src/me/waffletastic/SmokeTP/SmokeTP.java
+++ src/me/waffletastic/SmokeTP/SmokeTP.java
@@ -6,28 +6,27 @@ import java.util.logging.Logger;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SmokeTP extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
+	int smokesize = 3;
 	private final SmokeTPPlayerListener playerListener = new SmokeTPPlayerListener(this);
 	
 	public void onEnable() {
 		PluginManager pm = this.getServer().getPluginManager();
 		File configFile = new File(this.getDataFolder(),"config.yml");
 		FileConfiguration conf = YamlConfiguration.loadConfiguration(configFile);
-		conf.set("Smoke Size", 3);
-		conf.getInt("Smoke Size", 3);
+		smokesize = conf.getInt("Smoke Size", 3);
+		conf.set("Smoke Size", smokesize);
 		try {
 			conf.save(configFile);
 		} catch (IOException e) {
 			log.info("Could not save config.yml");
 		}
-		pm.registerEvent(Event.Type.PLAYER_TELEPORT, playerListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Priority.Normal, this);
+		
+		pm.registerEvents(playerListener, this);
 		
 		log.info("SmokeTP by waffletastic and Sluijsens enabled!");
 	}
--- src/me/waffletastic/SmokeTP/SmokeTPPlayerListener.java
+++ src/me/waffletastic/SmokeTP/SmokeTPPlayerListener.java
@@ -1,16 +1,15 @@
 package me.waffletastic.SmokeTP;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
-public class SmokeTPPlayerListener extends PlayerListener {
+public class SmokeTPPlayerListener implements Listener {
 	SmokeTP plugin;
 
 
@@ -18,16 +17,15 @@ public class SmokeTPPlayerListener extends PlayerListener {
 		this.plugin = instance;
 	}
 	
+	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
-		File configFile = new File(plugin.getDataFolder(),"config.yml");
-		FileConfiguration conf = YamlConfiguration.loadConfiguration(configFile);
 		if(event.getPlayer().hasPermission("smoketp.smoke")) {
 			List<Location> smokeLocations = new ArrayList<Location>();
 			smokeLocations.add(event.getFrom());
 			smokeLocations.add(event.getFrom().clone().add(0, 1, 0));
 			smokeLocations.add(event.getTo());
 			smokeLocations.add(event.getTo().clone().add(0, 1, 0));
-			SmokeUtil.spawnCloudRandom(smokeLocations, conf.getInt("Smoke Size", 3));
+			SmokeUtil.spawnCloudRandom(smokeLocations, plugin.smokesize);
 		}
 	}
 }
